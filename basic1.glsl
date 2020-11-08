0693086b8c413f2465e1b7e913ce831540d6925e
Akira Ogawa <aoo_74@hotmail.com>
2020-11-08 20:54:21 +0900

    init

--- basic1.glsl
+++ basic1.glsl
@@ -1,12 +1,24 @@
-#ifdef GL_ES
-precision mediump float;
-#endif
+void main() {
+  float time = iGlobalTime * 1.0;
+  vec2 uv = (gl_FragCoord.xy / iResolution.xx - 0.5) * 8.0;
+  vec2 uv0 = uv;
+  float i0 = 1.0;
+  float i1 = 1.0;
+  float i2 = 1.0;
+  float i4 = 0.0;
+  for (int s = 0; s < 7; s++) {
+    vec2 r;
+    r = vec2(cos(uv.y * i0 - i4 + time / i1), sin(uv.x * i0 - i4 + time / i1)) / i2;
+    r += vec2(-r.y, r.x) * 0.3;
+    uv.xy += r;
 
-uniform vec2 u_resolution;
-uniform vec2 u_mouse;
-uniform float u_time;
-
-void main(){
-	vec2 st = (gl_FragCoord.xy * 2. - u_resolution.xy) / min(u_resolution.x, u_resolution.y);
-	gl_FragColor = vec4(vec3(st.x, st.y, 0.), 1.0);
+    i0 *= 1.93;
+    i1 *= 1.15;
+    i2 *= 1.7;
+    i4 += 0.05 + 0.1 * time * i1;
+  }
+  float r = sin(uv.x - time) * 0.5 + 0.5;
+  float b = sin(uv.y + time) * 0.5 + 0.5;
+  float g = sin((uv.x + uv.y + sin(time * 0.5)) * 0.5) * 0.5 + 0.5;
+  gl_FragColor = vec4(r, g, b, 1.0);
 }
